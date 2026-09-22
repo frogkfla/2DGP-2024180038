503ba732ea332c36a20b72a285be822fec57c8b0
2025180014 <woorek@naver.com>
2026-09-22 16:40:27 +0900

    캔버스 열기 구현

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,1 +1,28 @@
 # 실습 과제 진행
+from pico2d import *
+
+open_canvas(800,600)
+
+
+def move_circle():
+    print("circle")
+    pass
+
+def move_rectangle():
+    print("rectangle")
+    pass
+
+def move_triangle():
+    print("triangle")
+    pass
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
+    pass    
+
+
+    break
+
+close_canvas()
